0154b89c2da79ac4ccf1366a46388cdf9513d375
Vera Ho <vh71886@gmail.com>
2022-06-14 12:05:44 -0400

    frontend setup

--- benchBNB/frontend.sh
+++ benchBNB/frontend.sh
@@ -1,7 +1,8 @@
-mkdir /frontend/actions
-mkdir /frontend/components
-mkdir /frontend/reducers
-mkdir /frontend/store
-mkdir /frontend/util
-touch /frontend/bench_bnb.jsx
+mkdir frontend/actions
+mkdir frontend/components
+mkdir frontend/reducers
+mkdir frontend/store
+mkdir frontend/util
+touch frontend/bench_bnb.jsx
 
+touch webpack.config.js
---
+++ benchBNB/npm_install.sh
@@ -0,0 +1,2 @@
+npm init -y
+npm install webpack webpack-cli react react-dom react-redux redux redux-logger @babel/core @babel/preset-react @babel/preset-env babel-loader
